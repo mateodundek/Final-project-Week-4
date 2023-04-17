822f08e9f126ee0fb4242fe37ba199224abaaf0c
mateodundek <mateo.dundek94@gmail.com>
2023-04-17 22:32:27 +0100

    filter_and_skeleton_loading_state

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
+let api
+let moviesData
+
 async function main() {
-  const movies = await fetch("https://www.omdbapi.com/?apikey=c510394&s=game");
-  const moviesData = await movies.json();
-  const movieListEl = document.querySelector(".movies");
-  console.log(moviesData);
-  movieListEl.innerHTML = moviesData.Search.slice(0,6).map(
-    (movie) => `<div class="search__results">
+   api = await fetch("https://www.omdbapi.com/?apikey=c510394&s=game");
+   moviesData = await api.json();
+   const movieListEl = document.querySelector(".movies");
+   
+   if(!api) {
+    api = moviesData 
+  }
+
+  if (filter === "year__low-to-high") {
+    moviesData.Search.sort((a, b) => a.Year.slice(0, 4) - b.Year.slice(0, 4));
+  } else if (filter === "year__high-to-low") {
+    moviesData.Search.sort((a, b) => b.Year.slice(0, 4) - a.Year.slice(0, 4));
+  }
+
+  moviesHTML = moviesData.Search.slice(0, 6)
+    .map(
+      (movie) => `<div class="search__results">
           <img class="result__img" src="${movie.Poster}" />
           <div class="results__para--description">
-            <p class="results__para"><span class="color__red">Title</span><br>${movie.Title}</p>
-            <p class="results__para"><span class="color__red">Year</span><br>${movie.Year}</p>
-            <p class="results__para"><span class="color__red">Type</span><br>${movie.Type}</p>
+            <p class="results__para"><span class="color__red">Title</span><br>${
+              movie.Title
+            }</p>
+            <p class="results__para"><span class="color__red">Year</span><br>${movie.Year.slice(
+              0,
+              4
+            )}</p>
+            <p class="results__para"><span class="color__red">Type</span><br>${
+              movie.Type
+            }</p>
           </div>
         </div>`
-  ).join("");
+    )
+    .join("");
+  movieListEl.innerHTML = moviesHTML;
 }
 
-main();
+function filterMovies(event) {
+  main(event.target.value);
+}
+
+setTimeout(() => {
+  main();
+}, 1000);
