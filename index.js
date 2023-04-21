d44d1fbb662ab531065a828de94a7982518c36c1
mateodundek <mateo.dundek94@gmail.com>
2023-04-21 15:23:20 +0100

    Search_bar_attempt

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const movieListEl = document.querySelector(".movies");
+const searchBar = document.getElementById("searchbar")
+let moviesData = []
+
+// searchBar.addEventListener("keyup", (e) => {
+//   const searchString = e.target.value
+//   const filteredMovies = moviesData.Search.filter( movie => {
+//     return movie.Title.includes(searchString)
+//   })
+//   main(filteredMovies)
+// })
+
+
 async function main(filter) {
   const api = await fetch("https://www.omdbapi.com/?apikey=c510394&s=game");
-  const moviesData = await api.json();
-  const movieListEl = document.querySelector(".movies");
+   moviesData = await api.json();
 
   if (filter === "year__old-to-new") {
     moviesData.Search.sort((a, b) => a.Year.slice(0, 4) - b.Year.slice(0, 4));
@@ -9,24 +21,27 @@ async function main(filter) {
     moviesData.Search.sort((a, b) => b.Year.slice(0, 4) - a.Year.slice(0, 4));
   }
 
-  moviesHTML = moviesData.Search.map(
-    (movie) => `<div class="search__results">
-          <img class="result__img" src="${movie.Poster}" />
-          <div class="results__para--description">
-            <p class="results__para"><span class="color__red">Title</span><br>${
-              movie.Title
-            }</p>
-            <p class="results__para"><span class="color__red">Year</span><br>${movie.Year.slice(
-              0,
-              4
-            )}</p>
-            <p class="results__para"><span class="color__red">Type</span><br>${
-              movie.Type
-            }</p>
-          </div>
-        </div>`
+  movieListEl.innerHTML = moviesData.Search.map((movie) =>
+    moviesHTML(movie)
   ).join("");
-  movieListEl.innerHTML = moviesHTML;
+}
+
+function moviesHTML(movie) {
+  return `<div class="search__results">
+  <img class="result__img" src="${movie.Poster}" />
+  <div class="results__para--description">
+    <p class="results__para"><span class="color__red">Title</span><br>${
+      movie.Title
+    }</p>
+    <p class="results__para"><span class="color__red">Year</span><br>${movie.Year.slice(
+      0,
+      4
+    )}</p>
+    <p class="results__para"><span class="color__red">Type</span><br>${
+      movie.Type
+    }</p>
+  </div>
+</div>`;
 }
 
 function filterMovies(event) {
